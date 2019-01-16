ab6eeb56774a6a833f816378d74b6b29c8827264
wildmoby <39716390+wildmoby@users.noreply.github.com>
2019-01-16 00:05:25 -0500

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,17 @@
+grades = list()
+
+while True:
+    firstname = raw_input("First Name: ")
+    if firstname == '':
+        break
+    lastname = raw_input("Last Name: ")
+    grade = raw_input("Grade: ")
+ 
+
+    my_tuple = (grade, lastname, firstname)
+    grades.append(my_tuple)
+
+grades.sort()
+
+for grade in grades:
+    print '  %-3s %-10s %-10s' % (grade [0], grade[1], grade[2])
